c297b49757287471b0e0cf1b3bcc3c20127ecf69
kcjia <otkinlife@163.com>
2024-02-23 18:40:04 +0800

    page

--- router.go
+++ router.go
@@ -6,6 +6,9 @@ import (
 )
 
 func Router(r *gin.Engine) {
+	// 静态文件
+	r.Static("/static", "./static")
+
 	r.GET("/ping", api.Ping)
 	r.POST("/load_weichat_bills", api.LoadWeiChatBills)
 	r.POST("/load_alipay_bills", api.LoadAliPayBills)
